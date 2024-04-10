c57ad9f643b51326246f52d6b6b30cbe3c7e3ced
Vova <vladimir@bagmanov.ru>
2024-04-10 21:18:57 +0300

    smth strange

---
+++ src/CommandFiller.java
@@ -0,0 +1,2 @@
+public class CommandFiller {
+}
--- src/Commands/Help.java
+++ src/Commands/Help.java
@@ -1,11 +1,12 @@
 package Commands;
 
 import java.util.ArrayList;
+import java.util.Objects;
 
 public class Help implements Command{
 
     @Override
-    public ArrayList<String> execute(ArrayList<String> args) {
+    public ArrayList<String> execute(ArrayList<Object> args) {
         ArrayList<String> response = new ArrayList<>();
         response.add("it's too late to help");
         return response;
