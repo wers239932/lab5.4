8147a652ab90135dcf5ac3a168fb51760823d321
Vova <vladimir@bagmanov.ru>
2024-04-12 22:19:04 +0300

    исправлены названия парсеров

--- src/cli/Commands/Add.java
+++ src/cli/Commands/Add.java
@@ -17,31 +17,31 @@ public class Add implements Command{
     public ArrayList<String> execute(ArrayList<String> args, Terminal terminal) throws CommandException {
 
         try {
-            Parser<String> parser = new Parser();
-            String name = parser.getArgumentWithRules("введите название города", terminal,
+            Parser<String> parserName = new Parser();
+            String name = parserName.getArgumentWithRules("введите название города", terminal,
                     arg -> City.parseName((String) arg));
-            Parser<Float> parser1 = new Parser();
-            float x = parser1.getArgumentWithRules("введите число в формате float, первую координату", terminal,
+            Parser<Float> parserX = new Parser();
+            float x = parserX.getArgumentWithRules("введите число в формате float, первую координату", terminal,
                     arg -> Coordinates.parseXCoord((String) arg));
-            Parser<Long> parser2 = new Parser();
-            long y = parser2.getArgumentWithRules("введите число в формате long, вторую координату", terminal,
+            Parser<Long> parserY = new Parser();
+            long y = parserY.getArgumentWithRules("введите число в формате long, вторую координату", terminal,
                     arg -> Coordinates.parseYCoord((String) arg));
-            Parser<Long> parser3 = new Parser();
-            Long area = parser3.getArgumentWithRules("введите площадь в формате Long, площадь должна быть больше 0", terminal,
+            Parser<Long> parserArea = new Parser();
+            Long area = parserArea.getArgumentWithRules("введите площадь в формате Long, площадь должна быть больше 0", terminal,
                     arg -> City.parseArea((String) arg));
-            Parser<Integer> parser4 = new Parser();
-            int population = parser4.getArgumentWithRules("введите население, должно быть больше 0", terminal, arg -> City.parsePopulation((String) arg));
-            Parser<Double> parser5 = new Parser();
-            double deep = parser5.getArgumentWithRules("введите высоту над уровнем моря в формате double", terminal, arg -> City.parseMetersAboveSeaLevel((String) arg));
-            Parser<Boolean> parser6 = new Parser();
-            Boolean capital  = parser6.getArgumentWithRules("введите true если у города есть столица, что угодно другое в ином случае", terminal, arg -> City.parseCapital((String) arg));
-            Parser<Long> parser7 = new Parser();
-            Long carcode = parser7.getArgumentWithRules("введите carCode - целое число от 0 до 1000", terminal, arg -> City.parseCarCode((String) arg));
-            Parser<Government> parser8 = new Parser();
-            Government government = parser8.getArgumentWithRules("введите тип правительства: KLEPTOCRACY, CORPORATOCRACY или PATRIARCHY", terminal, arg -> City.parseGovernment((String) arg));
-            Parser<Human> parser9 = new Parser();
-            Human guvernor = parser9.getArgumentWithRules("введите дату в формате yyyy-MM-dd<английская буква T>HH:mm:ss", terminal, arg -> Human.parseGovernor((String) arg));
-            City city = new City(name,new Coordinates(x,y),area,population,deep,capital,carcode,government, guvernor);
+            Parser<Integer> parserPopulation = new Parser();
+            int population = parserPopulation.getArgumentWithRules("введите население, должно быть больше 0", terminal, arg -> City.parsePopulation((String) arg));
+            Parser<Double> parserMetersAbove = new Parser();
+            double deep = parserMetersAbove.getArgumentWithRules("введите высоту над уровнем моря в формате double", terminal, arg -> City.parseMetersAboveSeaLevel((String) arg));
+            Parser<Boolean> parserCapital = new Parser();
+            Boolean capital  = parserCapital.getArgumentWithRules("введите true если у города есть столица, что угодно другое в ином случае", terminal, arg -> City.parseCapital((String) arg));
+            Parser<Long> parserCarCode = new Parser();
+            Long carcode = parserCarCode.getArgumentWithRules("введите carCode - целое число от 0 до 1000", terminal, arg -> City.parseCarCode((String) arg));
+            Parser<Government> parserGovernment = new Parser();
+            Government government = parserGovernment.getArgumentWithRules("введите тип правительства: KLEPTOCRACY, CORPORATOCRACY или PATRIARCHY", terminal, arg -> City.parseGovernment((String) arg));
+            Parser<Human> parserGovernor = new Parser();
+            Human governor = parserGovernor.getArgumentWithRules("введите дату в формате yyyy-MM-dd<английская буква T>HH:mm:ss", terminal, arg -> Human.parseGovernor((String) arg));
+            City city = new City(name,new Coordinates(x,y),area,population,deep,capital,carcode,government, governor);
             this.storage.add(city);
             ArrayList<String> response = new ArrayList<>();
             response.add("добавлен элемент");
