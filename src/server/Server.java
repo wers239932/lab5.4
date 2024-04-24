7e0184f932d03dc56f5daf52992ccd66f85e2b2a
Vova <vladimir@bagmanov.ru>
2024-04-24 15:20:16 +0300

    фиг его знает, вроде написал часть с приемом сообщения на сервере

--- src/server/Server.java
+++ src/server/Server.java
@@ -1,5 +1,7 @@
 package server;
 
+import api.Request;
+
 import java.io.*;
 import java.net.*;
 import java.nio.ByteBuffer;
@@ -9,9 +11,20 @@ public class Server {
     private DatagramChannel datagramChannel;
     private InetSocketAddress address;
     private SocketAddress clientSocket;
+    private DatagramSocket datagramSocket;
+    InetAddress hostAddress;
+
+    private
     public Server(int port)
     {
         try {
+            datagramSocket = new DatagramSocket(port);
+        } catch (SocketException e) {
+            throw new RuntimeException(e);
+        }
+
+
+        /*try {
             this.address = new InetSocketAddress(port);
             this.datagramChannel = DatagramChannel.open();
             this.datagramChannel.bind(address);
@@ -19,9 +32,30 @@ public class Server {
             System.out.println("Порт занят. Выберете другой порт");
         } catch (IOException e) {
             throw new RuntimeException(e);
-        }
+        }*/
     }
     public void handle(){
+        byte arr[] = new byte[60000];
+        DatagramPacket datagramPacket = new DatagramPacket(arr, 60000);
+        try {
+            this.datagramSocket.receive(datagramPacket);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        InetAddress clientAddress = datagramPacket.getAddress();
+
+        ByteArrayInputStream inputStream1 = new ByteArrayInputStream(arr);
+        Request request;
+        try {
+            ObjectInputStream inputStream = new ObjectInputStream(inputStream1);
+            request = (Request) inputStream.readObject();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+        String commandName = request.getCommandName();
+
         /*
         * читаем байты и объект реквест из udp
         * читаем имя команды
@@ -29,13 +63,21 @@ public class Server {
         * вызываем функцию соответствующую команде с нужными параметрами
         * */
     }
-    public void sendReply(Serializable object) throws IOException {
+    public void sendReply(Serializable object, InetAddress address, int port) throws IOException {
         ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
         ObjectOutputStream objectOutputStream = new ObjectOutputStream(byteArrayOutputStream);
         objectOutputStream.writeObject(object);
         objectOutputStream.flush();
         byte[] sendData = byteArrayOutputStream.toByteArray();
-        datagramChannel.send(ByteBuffer.wrap(sendData), clientSocket);
+        int len = sendData.length;
+        DatagramPacket datagramPacket = new DatagramPacket(sendData, len, address, port);
+        this.datagramSocket.send(datagramPacket);
+        /*ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        ObjectOutputStream objectOutputStream = new ObjectOutputStream(byteArrayOutputStream);
+        objectOutputStream.writeObject(object);
+        objectOutputStream.flush();
+        byte[] sendData = byteArrayOutputStream.toByteArray();
+        datagramChannel.send(ByteBuffer.wrap(sendData), clientSocket);*/
     }
     /*public Object getMessage()
     {
